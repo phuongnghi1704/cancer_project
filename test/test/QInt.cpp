afc32a9fbd4f096ecb964cc690ebe8133bb08fb2
NGUYỄN VĂN PHƯƠNG NGHI <1612415@student.hcmus.edu.vn>
2018-04-11 23:19:48 +0700

    .

--- test/test/QInt.cpp
+++ test/test/QInt.cpp
@@ -1,6 +1,62 @@
 #include "QInt.h"
 
+//Reference: https://codescracker.com/cpp/program/cpp-program-convert-hexadecimal-to-binary.htm
+string HexStringToBinString(string hex)
+{
+	string bin = "";
+	for (int i = 0; i < hex.length(); i++)
+	{
+		switch (toupper(hex[i]))
+		{
+		case '0': bin.append("0000"); break;
+		case '1': bin.append("0001"); break;
+		case '2': bin.append("0010"); break;
+		case '3': bin.append("0011"); break;
+		case '4': bin.append("0100"); break;
+		case '5': bin.append("0101"); break;
+		case '6': bin.append("0110"); break;
+		case '7': bin.append("0111"); break;
+		case '8': bin.append("1000"); break;
+		case '9': bin.append("1001"); break;
+		case 'A': bin.append("1010"); break;
+		case 'B': bin.append("1011"); break;
+		case 'C': bin.append("1100"); break;
+		case 'D': bin.append("1101"); break;
+		case 'E': bin.append("1110"); break;
+		case 'F': bin.append("1111"); break;
+		}
+	}
+	return bin;
+}
 
+string BinStringToBHexString(string bin)
+{
+	string hex = "";
+	string tmp = "0000";
+	for (int j = 0; j < 128; j += 4)
+	{
+		tmp = bin.substr(j, 4);
+		if (!tmp.compare("0000")) bin += "0";
+		else if (!tmp.compare("0001")) hex += "1";
+		else if (!tmp.compare("0010")) hex += "2";
+		else if (!tmp.compare("0011")) hex += "3";
+		else if (!tmp.compare("0100")) hex += "4";
+		else if (!tmp.compare("0101")) hex += "5";
+		else if (!tmp.compare("0110")) hex += "6";
+		else if (!tmp.compare("0111")) hex += "7";
+		else if (!tmp.compare("1000")) hex += "8";
+		else if (!tmp.compare("1001")) hex += "9";
+		else if (!tmp.compare("1010")) hex += "A";
+		else if (!tmp.compare("1011")) hex += "B";
+		else if (!tmp.compare("1100")) hex += "C";
+		else if (!tmp.compare("1101")) hex += "D";
+		else if (!tmp.compare("1110")) hex += "E";
+		else if (!tmp.compare("1111")) hex += "F";
+		else continue;
+	}
+
+	return hex;
+}
 
 QInt::QInt()
 {
